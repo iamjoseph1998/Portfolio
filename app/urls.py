59c45438d103ebfeadba4ceed945d72545219c41
iamjoseph1998 <iamjoseph1998@gmail.com>
2022-02-13 02:46:34 +0530

    Created experience html and added views and url

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+#register app for urls tag
+app_name = 'app'
+
 urlpatterns = [
     path('about/', views.about, name='about'),
+    path('experience/', views.experience, name='experience'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def about(request):
     return render(request, 'app/about.html')
+
+def experience(request):
+    return render(request, 'app/experience.html')
