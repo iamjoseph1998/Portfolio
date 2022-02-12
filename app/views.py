62259327f1bcfb2462f9f073d53ea01f581a27cb
iamjoseph1998 <iamjoseph1998@gmail.com>
2022-02-12 22:59:48 +0530

    Minor change for first demo html to work

--- app/views.py
+++ app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def about(request):
-    return render(request, 'templates/app/about.html')
+    return render(request, 'app/about.html')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,9 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
